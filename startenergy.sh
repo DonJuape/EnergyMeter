e85c59398925ca49f6d8ca6d2f3d7a2e524d679b
DonJuape <donjuape@gmail.com>
2021-01-23 16:28:55 +0100

    Added dashboard to startenergy script

--- startenergy.sh
+++ startenergy.sh
@@ -1,2 +1,3 @@
 sudo redis-server /etc/redis/6379.conf #Start redis server, requires redis-server to be installed
-sudo screen -dmS energy python3 ./EnergyMeter/energymeter.py #Start script as screen deamon, requires screen to be installed
+sudo screen -dmS energy python3 ./EnergyMeter/energymeter.py #Start energymeter script as screen deamon, requires screen to be installed
+sudo screen -dmS energy python3 ./Dashboard/dashboard.py #Start dashboard as screen deamon, requires screen to be installed
